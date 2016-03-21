34b483cd2a8a18132afaaec6181b9e84ee07d255
Shardquake <anniejacen6@hotmail.com>
2016-03-21 19:47:55 -0400

    Failed Table

--- main.lua
+++ main.lua
@@ -1,60 +1,58 @@
-function love.load()
+function loadmap()
 	TilesetEx = love.graphics.newImage("images/countryside.png")
 	TilesetGen = love.graphics.newImage("images/General.png")
 
-	local tileWEx, tileHEx = 32,32
+	tileWEx, tileHEx = 32,32
 	local tilesetWEx, tilesetHEx = TilesetEx:getWidth(), TilesetEx:getHeight()
-	local tileWGen, tileHGen = 32,32
-	local tilesetWGen, tilesetHGen = TilesetGen:getWidth(), TilesetGen:getHeight()
+	tileWGen, tileHGen = 32,32
+  local tilesetWGen, tilesetHGen = TilesetGen:getWidth(), TilesetGen:getHeight()
 
-	GrassQuad = love.graphics.newQuad(0,  0, tileWEx, tileHEx, tilesetWEx, tilesetHEx)
-  BoxQuad = love.graphics.newQuad(32, 0, tileWEx, tileHEx, tilesetWEx, tilesetHEx)
-  FlowerQuad = love.graphics.newQuad(0, 32, tileWEx, tileHEx, tilesetWEx, tilesetHEx)
-  WoodpanelQuad = love.graphics.newQuad(32, 32, tileWEx, tileHEx, tilesetWEx, tilesetHEx)
-	GrassGenQuad = love.graphics.newQuad(32, 352, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	SmStallLeftQuad = love.graphics.newQuad(448, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	SmStallRightQuad = love.graphics.newQuad(480, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	SmFruitStallLeftQuad = love.graphics.newQuad(448, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	SmFruitStallRightQuad = love.graphics.newQuad(480, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	TpLeftCobbleQuad = love.graphics.newQuad(0, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	TpCobbleQuad = love.graphics.newQuad(32, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	LeftCobbleQuad = love.graphics.newQuad(0, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
-	CobbleQuad = love.graphics.newQuad(32, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen)
+  Quads = {
+  	GrassQuad = love.graphics.newQuad(0,  0, tileWEx, tileHEx, tilesetWEx, tilesetHEx), -- old
+    BoxQuad = love.graphics.newQuad(32, 0, tileWEx, tileHEx, tilesetWEx, tilesetHEx), -- old
+    FlowerQuad = love.graphics.newQuad(0, 32, tileWEx, tileHEx, tilesetWEx, tilesetHEx), -- old
+    WoodpanelQuad = love.graphics.newQuad(32, 32, tileWEx, tileHEx, tilesetWEx, tilesetHEx), -- old
+  	GrassGenQuad = love.graphics.newQuad(32, 352, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 5 Grass
+  	SmStallLeftQuad = love.graphics.newQuad(448, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 6 Small Stall Left
+  	SmStallRightQuad = love.graphics.newQuad(480, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 7 Small Stall Right
+  	SmFruitStallLeftQuad = love.graphics.newQuad(448, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 8 Fruit Stall Left
+  	SmFruitStallRightQuad = love.graphics.newQuad(480, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 9 Fruit Stall Right
+  	TpLeftCobbleQuad = love.graphics.newQuad(0, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 10 Top Left Cobble
+  	TpCobbleQuad = love.graphics.newQuad(32, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 11 Top Cobble
+  	LeftCobbleQuad = love.graphics.newQuad(0, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 12 Left Cobble
+  	CobbleQuad = love.graphics.newQuad(32, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 13 Cobble
+  	TpRightCobbleQuad = love.graphics.newQuad(96, 0, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 14 Top Right Cobble
+  	RightCobbleQuad = love.graphics.newQuad(96, 32, tileWGen, tileHGen, tilesetWGen, tilesetHGen), -- 15 Right Cobble
+  }
+
+  TileTable = {
+
+  	{ 5, 6, 7, 5},
+  	{ 10, 8, 9, 14},
+  	{ 12, 13, 13, 15}
+
+	}
+
 end
 
+function drawmap()
+	for rowIndex=1, #TileTable do
+    local row = TileTable[rowIndex]
+    for columnIndex=1, #row do
+      local number = row[columnIndex]
+      local x = (columnIndex-1)*tileWGen
+      local y = (rowIndex-1)*tileHGen
+			print("test")
+			print(Quads[number])
+      love.graphics.draw(TilesetGen, Quads[number], x, y)
+    end
+  end
+end
 
+function love.load()
+	loadmap()
+end
 
 function love.draw()
-	love.graphics.draw(TilesetEx, GrassQuad, 0, 0)
-	love.graphics.draw(TilesetEx, FlowerQuad, 32, 0)
-	love.graphics.draw(TilesetEx, FlowerQuad, 0, 32)
-	love.graphics.draw(TilesetEx, WoodpanelQuad, 32, 32)
-	love.graphics.draw(TilesetEx, FlowerQuad, 64, 32)
-	love.graphics.draw(TilesetEx, FlowerQuad, 0, 64)
-	love.graphics.draw(TilesetEx, FlowerQuad, 64, 64)
-	love.graphics.draw(TilesetEx, GrassQuad, 64, 0)
-	love.graphics.draw(TilesetEx, GrassQuad, 64, 96)
-	love.graphics.draw(TilesetEx, GrassQuad, 0, 96)
-	love.graphics.draw(TilesetEx, BoxQuad, 32, 64)
-	love.graphics.draw(TilesetEx, FlowerQuad, 32, 96)
-
-	love.graphics.draw(TilesetGen, GrassGenQuad, 128, 128)
-	love.graphics.draw(TilesetGen, TpLeftCobbleQuad, 128, 160)
-	love.graphics.draw(TilesetGen, CobbleQuad, 160, 192)
-	love.graphics.draw(TilesetGen, LeftCobbleQuad, 128, 192)
-	love.graphics.draw(TilesetGen, CobbleQuad, 192, 192)
-	love.graphics.draw(TilesetGen, SmStallLeftQuad, 160, 128)
-	love.graphics.draw(TilesetGen, SmStallRightQuad, 192, 128)
-	love.graphics.draw(TilesetGen, SmFruitStallLeftQuad, 160, 160)
-	love.graphics.draw(TilesetGen, SmFruitStallRightQuad, 192, 160)
-
-	love.graphics.draw(TilesetEx, GrassQuad, 368, 268)
-	love.graphics.draw(TilesetEx, GrassQuad, 400, 268)
-  love.graphics.draw(TilesetEx, GrassQuad, 432, 268)
-  love.graphics.draw(TilesetEx, GrassQuad, 368, 300)
-  love.graphics.draw(TilesetEx, BoxQuad  , 400, 300)
-  love.graphics.draw(TilesetEx, GrassQuad, 432, 300)
-  love.graphics.draw(TilesetEx, GrassQuad, 368, 332)
-  love.graphics.draw(TilesetEx, GrassQuad, 400, 332)
-  love.graphics.draw(TilesetEx, GrassQuad, 432, 332)
+  drawmap()
 end
