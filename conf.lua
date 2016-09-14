577915ed6a8b2fb3ccfd9726ed7b93c8b3b42b41
Jay Thomas <jay@gfax.ch>
2016-09-14 09:41:57 -0400

    Clean up conf; Fix linting error

--- conf.lua
+++ conf.lua
@@ -1,26 +1,26 @@
-local gameTitle = "Vale"
+local game_title = 'Vale'
 
-local generateTitle = function(gameTitle)
+local generate_title = function(title)
   local time = os.time()
   math.randomseed(time)
   local table = {
-    "doodle",
-    "bob",
-    "dingus"
+    'doodle',
+    'bob',
+    'dingus'
   }
   local index = math.random(3)
-  return gameTitle .. ": " .. table[index]
+  return title .. ': ' .. table[index]
 end
 
 function love.conf(t)
   t.identity = nil                    -- The name of the save directory (string)
-  t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
+  t.version = '0.10.1'                -- The LÖVE version this game was made for (string)
   t.console = true                    -- Attach a console (boolean, Windows only)
   t.accelerometerjoystick = false     -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
   t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean)
   t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
-  t.window.title = generateTitle(gameTitle)    -- The window title (string)
+  t.window.title = generate_title(game_title)    -- The window title (string)
   t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
   t.window.width = 800                -- The window width (number)
   t.window.height = 600               -- The window height (number)
@@ -29,7 +29,7 @@ function love.conf(t)
   t.window.minwidth = 1               -- Minimum window width if the window is resizable (number)
   t.window.minheight = 1              -- Minimum window height if the window is resizable (number)
   t.window.fullscreen = false         -- Enable fullscreen (boolean)
-  t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
+  t.window.fullscreentype = 'desktop' -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
   t.window.vsync = true               -- Enable vertical sync (boolean)
   t.window.msaa = 0                   -- The number of samples to use with multi-sampled antialiasing (number)
   t.window.display = 1                -- Index of the monitor to show the window in (number)
@@ -43,8 +43,8 @@ function love.conf(t)
   t.modules.image = true              -- Enable the image module (boolean)
   t.modules.joystick = false          -- Enable the joystick module (boolean)
   t.modules.keyboard = true           -- Enable the keyboard module (boolean)
-  t.modules.math = false              -- Enable the math module (boolean)
-  t.modules.mouse = true              -- Enable the mouse module (boolean)
+  t.modules.math = true               -- Enable the math module (boolean)
+  t.modules.mouse = false             -- Enable the mouse module (boolean)
   t.modules.physics = true            -- Enable the physics module (boolean)
   t.modules.sound = false             -- Enable the sound module (boolean)
   t.modules.system = true             -- Enable the system module (boolean)
